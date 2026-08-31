406a68f62f411669f0d6f32d0cb0c772ccb4c138
saryuYYY <138548776+saryuYYY@users.noreply.github.com>
2026-08-31 15:03:41 +0900

    Fix tracker feed behind avatar

--- Assets/RealtimeBodyTracking/Scripts/TrackerVideoBackground.cs
+++ Assets/RealtimeBodyTracking/Scripts/TrackerVideoBackground.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.UI;
 
 namespace RealtimeBodyTracking
 {
@@ -12,27 +11,27 @@ namespace RealtimeBodyTracking
     {
         [SerializeField] private string frameUrl = "http://127.0.0.1:39543/frame.jpg";
         [SerializeField, Range(5, 60)] private int refreshRate = 30;
-        [SerializeField] private bool mirror = true;
-        private RawImage image;
+        [SerializeField] private bool mirror;
+        [SerializeField, Min(1f)] private float backgroundDistance = 100f;
+        private Camera targetCamera;
+        private Transform background;
+        private MeshRenderer backgroundRenderer;
         private Texture2D texture;
 
         private void Awake()
         {
-            var canvasObject = new GameObject("Tracker Video Background", typeof(Canvas));
-            var canvas = canvasObject.GetComponent<Canvas>();
-            canvas.renderMode = RenderMode.ScreenSpaceCamera;
-            canvas.worldCamera = GetComponent<Camera>() ?? Camera.main;
-            canvas.sortingOrder = -100;
-            var rect = new GameObject("Source Frame", typeof(RectTransform), typeof(RawImage));
-            rect.transform.SetParent(canvasObject.transform, false);
-            var transform = rect.GetComponent<RectTransform>();
-            transform.anchorMin = Vector2.zero;
-            transform.anchorMax = Vector2.one;
-            transform.offsetMin = Vector2.zero;
-            transform.offsetMax = Vector2.zero;
-            image = rect.GetComponent<RawImage>();
-            image.color = Color.white;
-            image.uvRect = mirror ? new Rect(1f, 0f, -1f, 1f) : new Rect(0f, 0f, 1f, 1f);
+            targetCamera = GetComponent<Camera>() ?? Camera.main;
+            var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            quad.name = "Tracker Video Background";
+            background = quad.transform;
+            background.SetParent(targetCamera.transform, false);
+            background.localPosition = Vector3.forward * backgroundDistance;
+            background.localRotation = Quaternion.identity;
+            Destroy(quad.GetComponent<Collider>());
+            backgroundRenderer = quad.GetComponent<MeshRenderer>();
+            var shader = Shader.Find("Unlit/Texture");
+            backgroundRenderer.material = new Material(shader);
+            backgroundRenderer.enabled = false;
             StartCoroutine(PollFrames());
         }
 
@@ -51,7 +50,14 @@ namespace RealtimeBodyTracking
                         {
                             if (texture != null) Destroy(texture);
                             texture = next;
-                            image.texture = texture;
+                            texture.wrapMode = TextureWrapMode.Clamp;
+                            backgroundRenderer.material.mainTexture = texture;
+                            backgroundRenderer.material.mainTextureScale =
+                                mirror ? new Vector2(-1f, 1f) : Vector2.one;
+                            backgroundRenderer.material.mainTextureOffset =
+                                mirror ? new Vector2(1f, 0f) : Vector2.zero;
+                            FitToSource(texture.width, texture.height);
+                            backgroundRenderer.enabled = true;
                         }
                     }
                 }
@@ -59,9 +65,32 @@ namespace RealtimeBodyTracking
             }
         }
 
+        private void FitToSource(int width, int height)
+        {
+            float viewHeight = 2f * backgroundDistance *
+                               Mathf.Tan(targetCamera.fieldOfView * Mathf.Deg2Rad * .5f);
+            float viewWidth = viewHeight * targetCamera.aspect;
+            float sourceAspect = (float)width / Mathf.Max(height, 1);
+            float fittedWidth;
+            float fittedHeight;
+            if (sourceAspect > targetCamera.aspect)
+            {
+                fittedWidth = viewWidth;
+                fittedHeight = viewWidth / sourceAspect;
+            }
+            else
+            {
+                fittedHeight = viewHeight;
+                fittedWidth = viewHeight * sourceAspect;
+            }
+            background.localScale = new Vector3(fittedWidth, fittedHeight, 1f);
+        }
+
         private void OnDestroy()
         {
             if (texture != null) Destroy(texture);
+            if (backgroundRenderer != null && backgroundRenderer.material != null)
+                Destroy(backgroundRenderer.material);
         }
     }
 }
